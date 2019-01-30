4431be0cc0fa219a22314f80949735779f934114
Mike Duke <aithon@gmail.com>
2019-01-29 19:10:34 -0700

    Basic server setup. Export setup on team-data

---
+++ duke-server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const teamData = require('./nba-team-data');
+
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'BYOB';
+app.locals.teamData = teamData;
+
+app.get('/', (request, response) => {
+  response.status(200).send('Hello World!');
+});
+
+app.listen(app.get(port), () => {
+  console.log(`${app.locals.title} is running on port ${app.get('port')}`)
+});
+
+module.exports = app;
