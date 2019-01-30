6a1cbe49adc3ec71b83b4941d84b07a73c24c9a1
Mike Duke <aithon@gmail.com>
2019-01-29 22:14:55 -0700

    Add mock-team-data file for testing. GET tests for teams passing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const teamData = require('./nba-team-data');
+const teamData = require('./test/mock-team-data');
 
 app.use(bodyParser.json());
 
@@ -11,6 +11,24 @@ app.locals.teamData = teamData;
 
 app.get('/', (request, response) => {
   response.status(200).send('Hello World!');
+});
+
+app.get('/api/v1/teams', (request, response) => {
+  response.status(200).json(app.locals.teamData);
+});
+
+app.get('/api/v1/teams/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  const foundTeam = app.locals.teamData.find((team) => {
+    return team.id === id;
+  });
+
+  if(foundTeam) {
+    response.status(200).json(foundTeam);
+  } else {
+    response.status(404).json({error: 'There are no entries with that id'});
+  }
+
 });
 
 app.listen(app.get('port'), () => {
