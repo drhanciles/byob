71e0bc56ea6ab6f5cc146cb27ec3ca40761a6764
Loryn Mason, Ashley Levi <masonloryn@gmail.com, ashplevi@gmail.com>
2019-02-05 10:30:19 -0700

    Reorganized server.js by endpoints

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const app = express();
 const bodyParser = require('body-parser');
-
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile.js')[environment];
 const database = require('knex')(configuration);
@@ -28,34 +26,6 @@ app.get('/api/v1/teams/:id', (request, response) => {
     .then((team) => {
       if (team.length > 0) {
         response.status(200).json(team);
-      } else {
-        response.status(404).json({ error: 'There are no entries with that id' });
-      }
-    })
-    .catch(error => response.status(500).json({ error }));
-});
-
-app.get('/api/v1/players', (request, response) => {
-  const key = Object.keys(request.query)[0];
-  const value = request.query[key];
-
-  if (key) {
-    database('players').where(key, value).select()
-      .then(players => response.status(200).json(players))
-      .catch(error => response.status(500).json({ error }));
-  } else {
-    database('players').select()
-      .then(players => response.status(200).json(players))
-      .catch(error => response.status(500).json({ error }));
-  }
-});
-
-app.get('/api/v1/players/:id', (request, response) => {
-  const id = parseInt(request.params.id, 10);
-  database('players').where('id', id).select()
-    .then((player) => {
-      if (player.length > 0) {
-        response.status(200).json(player);
       } else {
         response.status(404).json({ error: 'There are no entries with that id' });
       }
@@ -111,14 +81,29 @@ app.delete('/api/v1/teams/:id', (request, response) => {
     });
 });
 
-app.delete('/api/v1/players/:id', (request, response) => {
+app.get('/api/v1/players', (request, response) => {
+  const key = Object.keys(request.query)[0];
+  const value = request.query[key];
+
+  if (key) {
+    database('players').where(key, value).select()
+      .then(players => response.status(200).json(players))
+      .catch(error => response.status(500).json({ error }));
+  } else {
+    database('players').select()
+      .then(players => response.status(200).json(players))
+      .catch(error => response.status(500).json({ error }));
+  }
+});
+
+app.get('/api/v1/players/:id', (request, response) => {
   const id = parseInt(request.params.id, 10);
-  database('players').where('id', id).del()
-    .then((result) => {
-      if (result) {
-        response.status(200).json({ id });
+  database('players').where('id', id).select()
+    .then((player) => {
+      if (player.length > 0) {
+        response.status(200).json(player);
       } else {
-        response.status(404).json({ error: result.statusText });
+        response.status(404).json({ error: 'There are no entries with that id' });
       }
     })
     .catch(error => response.status(500).json({ error }));
@@ -150,6 +135,19 @@ app.patch('/api/v1/players/:id', (request, response) => {
         return response.status(200).send(`Player at id ${playerId} has been updated`);
       }
       return response.status(422).send('There is no player at that id');
+    })
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.delete('/api/v1/players/:id', (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  database('players').where('id', id).del()
+    .then((result) => {
+      if (result) {
+        response.status(200).json({ id });
+      } else {
+        response.status(404).json({ error: result.statusText });
+      }
     })
     .catch(error => response.status(500).json({ error }));
 });
