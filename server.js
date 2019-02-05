ef52b2750efdb7371ba72cd71ed9f077f6959886
Loryn Mason, Ashley Levi <masonloryn@gmail.com, ashplevi@gmail.com>
2019-02-05 10:39:48 -0700

    Fix lint error

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
+
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile.js')[environment];
 const database = require('knex')(configuration);
